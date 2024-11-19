7d809376e5bb714abda4b205d2f648039a39ce34
Marisel <shenapam3@gmail.com>
2024-11-19 13:53:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ async function startnigg(phone) {
           await xlicon.sendMessage(
             xlicon.user.id,
             {
-              text: 'HELLO THERE! 👋 \n\nDO NOT SHARE YOUR SESSION ID WITH ANYONE.\n\nPUT THE ABOVE IN SESSION_ID VAR\n\nTHANKS FOR USING SILENT-SOBX-MD BOT\n\n JOIN SUPPORT CHANNEL:-https://whatsapp.com/channel/0029VaHO5B0G3R3cWkZN970s \n',
+              text: 'Hey \n\nDo Not Share Your Session Id With Anyone.\n\nSupport  by Following Channel:-https://whatsapp.com/channel/0029Vajvy2kEwEjwAKP4SI0x \n',
             },
             { quoted: guru }
           )
